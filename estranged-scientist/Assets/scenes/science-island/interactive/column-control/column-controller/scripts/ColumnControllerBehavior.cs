60f155058d1989b45b6cb237d2c8327937c9576f
Joshua Sims <JoshuaSimsVA@gmail.com>
2017-03-19 15:43:46 -0400

    Columns and ColumnControllers placed

---
+++ estranged-scientist/Assets/scenes/science-island/interactive/column-control/column-controller/scripts/ColumnControllerBehavior.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColumnControllerBehavior : MonoBehaviour
+{
+    // 27.5
+    private bool active;
+
+    public bool Active
+    {
+        get
+        {
+            return active;
+        }
+        set
+        {
+            active = value;
+        }
+    }
+
+    public void Activate()
+    {
+
+    }
+
+    public void Deactivate()
+    {
+
+    }
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
