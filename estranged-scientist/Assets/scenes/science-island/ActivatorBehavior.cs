62dfdfea7227d632fef749468909fbe17dd385c2
Joshua Sims <JoshuaSimsVA@gmail.com>
2017-03-20 19:21:31 -0400

    Introduced Shiftable

---
+++ estranged-scientist/Assets/scenes/science-island/ActivatorBehavior.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActivatorBehavior : MonoBehaviour
+{
+    public GameObject larry;
+
+    public Shiftable shiftableColumn;
+
+    public Shiftable shiftableButton;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject == larry 
+            && Input.GetButtonDown("Activate"))
+        {
+            shiftableColumn.toggleShifting();
+            shiftableButton.toggleShifting();
+        }
+    }
+}
---
+++ estranged-scientist/Assets/scenes/science-island/Shiftable.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shiftable : MonoBehaviour
+{
+    private bool shifting;
+
+    public float speed;
+
+    private Vector3 initialPosition;
+
+    public float maxShiftedLocalPositionX;
+
+    public float maxShiftedLocalPositionY;
+
+    public float maxShiftedLocalPositionZ;
+
+    private Vector3 maxShiftedLocalPosition;
+
+    private void Awake()
+    {
+        initialPosition = transform.position;
+
+        maxShiftedLocalPosition = new Vector3(
+            maxShiftedLocalPositionX,
+            maxShiftedLocalPositionY,
+            maxShiftedLocalPositionZ);
+    }
+
+    internal void toggleShifting()
+    {
+        shifting = !shifting;
+    }
+
+    protected void Shift()
+    {
+        if (shifting == true)
+        {
+            transform.localPosition = Vector3.MoveTowards(
+                transform.localPosition,
+                maxShiftedLocalPosition,
+                speed * Time.deltaTime);
+        }
+
+        else if (shifting == false)
+        {
+            transform.localPosition = Vector3.MoveTowards(
+                transform.localPosition,
+                initialPosition,
+                speed * Time.deltaTime);
+        }
+    }
+}
