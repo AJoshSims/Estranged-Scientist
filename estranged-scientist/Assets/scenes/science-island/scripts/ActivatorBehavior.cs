3b353f30cc110c9906289bd7d9b47ede9a64096a
Joshua Sims <JoshuaSimsVA@gmail.com>
2017-03-21 00:31:27 -0400

    Completed all

---
+++ estranged-scientist/Assets/scenes/science-island/scripts/ActivatorBehavior.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Characters.FirstPerson;
+
+/// <summary>
+/// Defines the behavior of the activator objects which server as the
+/// interface to the interactions in this game.
+/// </summary>
+public class ActivatorBehavior : MonoBehaviour
+{
+    ///<summary>
+    /// The character whom the player is controlling from first person
+    /// perspective.
+    /// </summary>
+    private GameObject larry;
+
+    /// <summary>
+    /// The object which is shifted when the activator is interacted with.
+    /// </summary>
+    public Shift shiftObject;
+
+    /// <summary>
+    /// The button atop the activator which shifts upon interaction with the
+    /// activator.
+    /// </summary>
+    public Shift button;
+
+    /// <summary>
+    /// Gets Larry so that the activator can know when he is close by.
+    /// </summary>
+    private  void Awake()
+    {
+        larry = GameObject.Find("Larry");
+    }
+
+    /// <summary>
+    /// When Larry is close enough to the activator, and when Larry presses
+    /// the Activate button, the shiftable object will shift and the button
+    /// will as well.
+    /// </summary>
+    private void Update()
+    {
+        if (((button.transform.position - larry.transform.position).magnitude 
+            < 2) 
+            && Input.GetButtonDown("Activate"))
+        {
+            shiftObject.ToggleShifting();
+            button.ToggleShifting();
+        }
+    }
+}
---
+++ estranged-scientist/Assets/scenes/science-island/scripts/Shift.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Allows an object to be shifted away from and back to its original 
+/// position.
+/// </summary>
+public class Shift : MonoBehaviour
+{
+    /// <summary>
+    /// Is the object currently shifting away from its original position.
+    /// </summary>
+    private bool shiftingAway;
+
+    /// <summary>
+    /// The speed at which the object shifts.
+    /// </summary>
+    public float speed;
+
+    /// <summary>
+    /// The original position of the object.
+    /// </summary>
+    private Vector3 originalLocalPosition;
+
+    /// <summary>
+    /// The maximum X position that the object may shift to.
+    /// </summary>
+    public float maxShiftedLocalPositionX;
+
+    /// <summary>
+    /// The maximum Y position that the object may shift to.
+    /// </summary>
+    public float maxShiftedLocalPositionY;
+
+    /// <summary>
+    /// The maximum Z position that the object may shift to.
+    /// </summary>
+    public float maxShiftedLocalPositionZ;
+
+    /// <summary>
+    /// The maximum position that the object may shift to.
+    /// </summary>
+    private Vector3 maxShiftedLocalPosition;
+
+    /// <summary>
+    /// Establishes the object's original position, that it is not shifting
+    /// away yet, and what its maximum shifted position is.
+    /// </summary>
+    private void Awake()
+    {
+        shiftingAway = false;
+
+        originalLocalPosition = transform.localPosition;
+
+        maxShiftedLocalPosition = new Vector3(
+            maxShiftedLocalPositionX,
+            maxShiftedLocalPositionY,
+            maxShiftedLocalPositionZ);
+    }
+
+    /// <summary>
+    /// Provokes the object to shift either away from or back to its original
+    /// position.
+    /// </summary>
+    internal void ToggleShifting()
+    {
+        shiftingAway = !shiftingAway;
+    }
+
+    /// <summary>
+    /// Shifts the object either away from or back to its original position.
+    /// </summary>
+    protected void Update()
+    {
+        if (shiftingAway == true)
+        {
+            transform.localPosition = Vector3.MoveTowards(
+                transform.localPosition,
+                maxShiftedLocalPosition,
+                speed * Time.deltaTime);
+        }
+
+        else if (shiftingAway == false)
+        {
+            transform.localPosition = Vector3.MoveTowards(
+                transform.localPosition,
+                originalLocalPosition,
+                speed * Time.deltaTime);
+        }
+    }
+}
